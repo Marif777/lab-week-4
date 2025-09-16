042676fb2514f38a1e4e7a42bfcf61127a7ecce6
Mariyum. A <maar1149687@stu.sjcc.edu>
2025-09-15 22:09:11 -0400

    task 5 done

---
+++ task5.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        char spice = 'Q';
+        float piNumber = 3.141592f;
+        double piLonger = 3.141592653589793;
+
+        System.out.println("Char: " + spice);
+        System.out.println("Float: " + piNumber);
+        System.out.println("Double: " + piLonger);
+    }
+}
