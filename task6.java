d7e0d24ea1606a6a3e57d837d635dce1a08cb6e6
Mariyum. A <maar1149687@stu.sjcc.edu>
2025-09-15 22:15:36 -0400

    task 6

---
+++ task6.java
@@ -0,0 +1,9 @@
+public class task6 {
+    public static void main(String[] args) {
+        Integer verseNumber = 786;
+        Float gravityConstant = 9.81f;
+
+        System.out.println("Integer: " + verseNumber);
+        System.out.println("Float: " + gravityConstant);
+    }
+}
