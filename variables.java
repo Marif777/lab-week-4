cb15b29924692e19720eaaa0d37a476395aef62c
Mariyum. A <maar1149687@stu.sjcc.edu>
2025-09-15 21:57:57 -0400

    done task 3

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstInt = 777;
+        short secondInt = 666;
+        long longDelta = 9999L;
+        byte strawberryBytes = 127;
+        boolean isAssignmentEasy = true;
+
+        System.out.println("Integer: " + firstInt);
+        System.out.println("Short: " + secondInt);
+        System.out.println("Long: " + longDelta);
+        System.out.println("Byte: " + strawberryBytes);
+        System.out.println("Is assignment easy? " + isAssignmentEasy);
+    }
+}
